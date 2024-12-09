be6903e3cc6d9d3e80e980192e742f1f07825e02
khoirulmuasin <gabutcrypto1@gmail.com>
2024-12-10 04:58:51 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+
+export default function app() {
+    return (
+        <view style={{ flex: 1, backgroundColor: '#282c34' }}>
+            <statusbar style='light' />
+
+            <view style={{ flex: 1. backgroundColo: 'red' }}></view>
+
+            <view style={{ flex: 1, backgroundColor: 'blue' }}></view>
+
+            <view />
+
+            );
+}
+
+            const styles = StyleSheet.create({
+                container: {
+                flex: 1,
+            backgroundColor: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+    },
+});
