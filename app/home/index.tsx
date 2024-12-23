7f2e5ac89ea5abbb9be5b7614f992f882f3078dd
khoirulmuasin <gabutcrypto1@gmail.com>
2024-12-23 22:15:19 +0700

    home

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <Text>HOME</Text>
             <Link
                 href={{
                     pathname: '/details/[id]',
